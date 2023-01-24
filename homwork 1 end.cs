19751d44035b80ae0509993028f9fda6cf32b9a9
ariannanicole24 <123431265+ariannanicole24@users.noreply.github.com>
2023-01-23 22:09:50 -0600

    homework 1 code file
    I believe the first one under "Prpgram.cs" will not work because after I submitted it, I changed the name and deleted it, I was getting an error until the wrong project, so here I do not have that error.

---
+++ homwork 1 end.cs
@@ -0,0 +1,18 @@
+﻿namespace HOMEWORK1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //giving my datatypes values
+        double X = 4*2.5*2.5;
+        double Y = 3*3.3;
+
+        //the calculation to get the end result
+        double Z = X+Y;
+        
+
+        //end result
+        Console.WriteLine("X = 2.5 , Y = 3.3");
+        Console.WriteLine($"The value of Z is : " +Z);
+    }
+}
