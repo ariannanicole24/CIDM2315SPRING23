350d523a4d74923a78135aa10b28a1fffda83fb1
ariannanicole24 <123431265+ariannanicole24@users.noreply.github.com>
2023-02-27 02:44:17 -0600

    Add files via upload

---
+++ homework 2.cs
@@ -0,0 +1,39 @@
+﻿namespace Lecture2._0;
+class Program
+{
+    static void Main(){
+    {
+        Console.WriteLine($"Please input letter grade: ");
+        char letter = Console.ReadLine() [0];
+
+    
+        switch(letter)
+        {
+         case 'A':
+         Console.WriteLine("GPA point: 4");
+         break;
+
+          case 'B':
+         Console.WriteLine("GPA point: 3");
+         break;
+
+          case 'C':
+         Console.WriteLine("GPA point: 2");
+         break;
+
+          case 'D':
+         Console.WriteLine("GPA point: 1");
+         break;
+
+         case 'F':
+         Console.WriteLine("GPA point: 0");
+         break;
+
+         default: Console.WriteLine("Wrong Letter Grade!");
+         break;
+        }
+        
+        
+    }
+    }
+}
