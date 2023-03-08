fc552373731656bbfd8a3ba16db13ff4212a1cc3
ariannanicole24 <123431265+ariannanicole24@users.noreply.github.com>
2023-03-08 02:20:15 -0600

    Add files via upload
    homework 3

--- Program.cs
+++ Program.cs
@@ -1,18 +1,42 @@
-﻿namespace HOMEWORK1;
+﻿namespace Lecture3._0;
 class Program
 {
     static void Main(string[] args)
-    {
-        //giving my datatypes values
-        double X = 4*2.5*2.5;
-        double Y = 3*3.3;
+        {
+        bool prime = true;
+        Console.WriteLine("Input an integer: ");
 
-        //the calculation to get the end result
-        double Z = X+Y;
-        
+        string user_input = Console.ReadLine();
+        int number = Convert.ToInt32(user_input);
 
-        //end result
-        Console.WriteLine("X = 2.5 , Y = 3.3");
-        Console.WriteLine($"The value of Z is : " +Z);
+        for(int i=2; i<number/2;i++)
+        {
+            if(number%i==0)
+            {
+                prime = false;
+                break;
+            }
+        }
+        if(prime)
+        {
+            Console.WriteLine( user_input +" is Prime");
+        }
+        else{
+            Console.WriteLine( user_input +" is non-prime");
+        }
+
+
+        //question 2
+        Console.WriteLine("Assign an int value to N: ");
+
+        int N_input = Convert.ToInt16(Console.ReadLine());
+
+        for(int i = 0;i<N_input;i++){
+
+            for(int j =0;j<N_input;j++){
+                Console.Write("#");
+            }
+            Console.WriteLine();
+        }
     }
 }
