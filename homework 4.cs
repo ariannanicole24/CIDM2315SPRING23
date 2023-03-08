cb15f06de678da8d937a9fecdabbcda3fe74a0e0
ariannanicole24 <123431265+ariannanicole24@users.noreply.github.com>
2023-03-08 03:16:49 -0600

    Add files via upload
    upload homework 4

---
+++ homework 4.cs
@@ -0,0 +1,25 @@
+﻿namespace Lecture4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numone, numtwo, maxnum;
+
+        //num1 input
+        Console.Write("a = ");
+        string user_input = Console.ReadLine();
+        numone = Convert.ToInt16(user_input);
+        //num2 input
+        Console.Write("b = ");
+        string userinput = Console.ReadLine();
+        numtwo = Convert.ToInt16(userinput);
+        
+        if(numone > numtwo){
+            maxnum = numone;
+        }
+        else{
+            maxnum = numtwo;
+        }
+        Console.WriteLine($"The largest number is: {maxnum}");
+    }
+}
