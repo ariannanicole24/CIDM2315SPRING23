0f82662c83e2ccaa12b67e832cf8c5f5739a9ac7
ariannanicole24 <123431265+ariannanicole24@users.noreply.github.com>
2023-03-17 00:37:33 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,42 +1,71 @@
-﻿namespace Lecture3._0;
+﻿using System;
+
+class Professor
+{
+    public string profName;
+    public string classteach;
+    private double salary;
+
+    public void givenSalary(double salary_amount)
+    {
+        salary = salary_amount;
+    }
+    public double GetSalary()
+    {
+        return salary;
+    }
+}
+
+class Student
+{
+    public string studentName;
+    public string ClassEnroll;
+    private double studentGrade;
+
+    public void SetStudentGrade(double newGrade)
+    {
+        studentGrade = newGrade;
+    }
+    public double GetStudentGrade()
+    {
+        return studentGrade();
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
-        {
-        bool prime = true;
-        Console.WriteLine("Input an integer: ");
+    static void Main()
+    {
+        Professor pf1 = new Professor();
+        pf1.profName= "Alice";
+        pf1.classTeach = "Java";
+        pf1.SetSalary(9000.0);
 
-        string user_input = Console.ReadLine();
-        int number = Convert.ToInt32(user_input);
+        Professor pf2 = new Professor();
+        pf2.profName = "Bob";
+        pf2.classTeach = "Math";
+        pf2.SetSalary(8000.0);
 
-        for(int i=2; i<number/2;i++)
-        {
-            if(number%i==0)
-            {
-                prime = false;
-                break;
-            }
-        }
-        if(prime)
-        {
-            Console.WriteLine( user_input +" is Prime");
-        }
-        else{
-            Console.WriteLine( user_input +" is non-prime");
-        }
+        Student st1 = new Student();
+        st1.studentName = "Lisa";
+        st1.ClassEnroll = "Java";
+        st1.SetStudentGrade(90.0);
 
+        Student st2 = new Student();
+        st2.studentName = "Tom";
+        st2.ClassEnroll = "Math";
+        st2.SetStudentGrade(80.0);
 
-        //question 2
-        Console.WriteLine("Assign an int value to N: ");
+        Console.WriteLine("Professor " + pf1.profName + " teaches " + pf1.classTeach + ", and the salary is: " + pf1.GetSalary());
 
-        int N_input = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Professor " + pf2.profName + " teaches " + pf2.classteach + ", and the salary is; " + pf2.GetSalary());
 
-        for(int i = 0;i<N_input;i++){
+        Console.WriteLine("Student " + st1.studentName + " enrolls " + st1.ClassEnroll + ", and the grade is: " + st2.GetStudentGrade());
 
-            for(int j =0;j<N_input;j++){
-                Console.Write("#");
-            }
-            Console.WriteLine();
-        }
+        Console.WriteLine("Student " + st2.studentName + " enrolls " + st2.ClassEnroll + ", and the grade is:: " + st2.GetStudentGrade ());
+
+        Console.WriteLine("The salary difference between " + pf1.profName + " and " + pf2.profName + " is, " + (pf1.GetSalary() - pf2.GetSalary()));
+
+        Console.WriteLine("The total grade of " + " is; " + (st1.GetStudentGrade() + st2.GetStudentGrade())); 
     }
 }
